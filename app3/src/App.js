80096c957505ad3bf2ccc7e9903553e26920a79a
Madison Walmsley <madi@Madisons-MacBook-Pro.local>
2018-10-23 11:04:12 -0700

    debugged app3

--- app3/src/App.js
+++ app3/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Movie from 'Movie';
+import Movie from './Movie';
 
 class App extends Component {
 
@@ -15,13 +14,15 @@ class App extends Component {
   render() {
 
     const badMovies = this.state.badMovies.map( movie => {
+      return(
       <Movie movie={movie} />
+      )
     })
 
     return (
       <div className="App">
         <h1>Terrible Movies List:</h1>
-        [badMovies]
+        {badMovies}
       </div>
     );
   }
